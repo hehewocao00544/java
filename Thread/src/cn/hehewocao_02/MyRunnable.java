31eaee575f11ff2f4fd39cb7963e16b210349caa
hehewocao00544 <2495399053@qq.com>
2019-04-20 16:07:42 +0800

    多线程模拟窗口售票,线程安全问题

--- Thread/src/cn/hehewocao_02/MyRunnable.java
+++ Thread/src/cn/hehewocao_02/MyRunnable.java
@@ -1,18 +1,31 @@
 package cn.hehewocao_02;
 
-public class MyRunnable implements Runnable{
+public class MyRunnable implements Runnable {
 
 	private int sum = 0;
-	
+	private Object obj = new Object();
+
 	public MyRunnable() {
-		
+
 	}
-	
+
 	public void run() {
-		
-		while(sum < 100) {
-			System.out.println(Thread.currentThread().getName() + "正在售卖第" + (++sum) + "张票!");
+
+		while (true) {
+			synchronized (obj) {
+				if (sum < 100) {
+					try {
+						Thread.sleep(200);
+					} catch (InterruptedException e) {
+						// TODO 自动生成的 catch 块
+						System.out.println("延时失败！");
+					}
+					System.out.println(Thread.currentThread().getName() + "正在售卖第" + (++sum) + "张票!");
+				}else {
+					System.out.println(Thread.currentThread().getName() + "票已售空！");
+					break;
+				}
+			}
 		}
-		System.out.println(Thread.currentThread().getName() + "已售空！");
 	}
 }
--- Thread/src/cn/hehewocao_02/MyThread.java
+++ Thread/src/cn/hehewocao_02/MyThread.java
@@ -4,16 +4,23 @@ public class MyThread extends Thread {
 
 	static int sum = 0;
 
+	private Object obj = new Object();
+
 	public void run() {
 
-		while (sum < 100) {
+		while(true) {
 			
-				System.out.println(getName() + "正在售卖第" + (++sum) + "张票");
-			
+			synchronized(obj) {
+				if (sum < 100) {
+					System.out.println(getName() + "正在售卖第" + (++sum) + "张票!");
+				}else {
+					System.out.println(getName() + "票已售空！");
+					break;
+				}
+				
+			}
 		}
-		if(sum == 100){
-			System.out.println(getName() + "票已售空！");
-		}
+
 	}
 
 	public MyThread() {
